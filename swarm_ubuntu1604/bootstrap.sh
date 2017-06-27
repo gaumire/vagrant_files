170703c1a95a47c6da8eb543cadd9bd546e518a2
Gaurav Ghimire <gaurav.ghimire@gmail.com>
2017-06-28 08:17:46 +1000

    added swarm cluster

---
+++ swarm_ubuntu1604/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+cp /vagrant/provision/docker.list /etc/apt/sources.list.d/.
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r) docker-engine
+usermod -aG docker ubuntu
+service docker restart
